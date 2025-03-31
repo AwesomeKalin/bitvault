92a2fed85baefb89158d7d835899637c5414e840
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2025-03-31 20:07:33 +0100

    Fixed address getter and switch back to using mnemonic to get address rather than hardcode address

--- src/lib/addresses.ts
+++ src/lib/addresses.ts
@@ -1,7 +1,7 @@
 import { HD, Mnemonic } from "@bsv/sdk";
 import { getSeed } from "./runes.svelte";
 import { createSPV, getSPV } from "./spv-store";
-import { TxoLookup } from "spv-store";
+import { OneSatWebSPV, TxoLookup } from "spv-store";
 
 export async function getNextAddress() {
     const hdWallet: HD = HD.fromSeed(new Mnemonic(getSeed()).toSeed());
@@ -14,8 +14,15 @@ export async function getNextAddress() {
         createSPV(address);
         const spv = getSPV(address);
 
-        if ((await spv.getRecentTxs()).length === 0) return address;
-        nextCheck++;
+        await spv.sync();
+
+        // Check for any type of transaction
+        if (await checkIfAddressUsed(spv)) {
+            nextCheck++;
+            continue;
+        }
+
+        return address;
     }
 }
 
@@ -27,25 +34,41 @@ export async function getBSVBalance() {
     let balance: number = 0;
 
     while (true) {
-        //const address: string = hdWallet.derive(`${basePath}/${nextCheck}`).privKey.toAddress();
-        const address: string = '142dcg1Un4u6na2sR9gTgaB5QybqE4sZbT';
+        const address: string = hdWallet.derive(`${basePath}/${nextCheck}`).privKey.toAddress();
         createSPV(address);
         const spv = getSPV(address);
-        console.log('1');
 
         await spv.sync();
 
-        console.log('2');
+        if (!(await checkIfAddressUsed(spv))) return balance;
 
         const txos = (await spv.search(new TxoLookup('fund'), undefined, 0)).txos;
-        console.log('3');
 
         for (let i = 0; i < txos.length; i++) {
             balance += parseInt(txos[i].satoshis.toString());
         }
 
         nextCheck++;
-
-        return balance;
     }
+}
+
+async function tagCheck(spv: OneSatWebSPV, tag: string): Promise<boolean> {
+    return (await spv.search(new TxoLookup(tag, undefined, undefined, undefined, true), undefined, 0)).txos.length > 0;
+}
+
+async function checkIfAddressUsed(spv: OneSatWebSPV): Promise<boolean> {
+    if (await tagCheck(spv, '1sat')) return true;
+    if (await tagCheck(spv, 'bsv20')) return true;
+    if (await tagCheck(spv, 'bsv21')) return true;
+    if (await tagCheck(spv, 'cosign')) return true;
+    if (await tagCheck(spv, 'fund')) return true;
+    if (await tagCheck(spv, 'insc')) return true;
+    if (await tagCheck(spv, 'lock')) return true;
+    if (await tagCheck(spv, 'map')) return true;
+    if (await tagCheck(spv, 'opns')) return true;
+    if (await tagCheck(spv, 'list')) return true;
+    if (await tagCheck(spv, 'origin')) return true;
+    if (await tagCheck(spv, 'sigma')) return true;
+
+    return false;
 }
--- src/lib/spv-store.ts
+++ src/lib/spv-store.ts
@@ -1,4 +1,4 @@
-import { Bsv20Indexer, Bsv21Indexer, FundIndexer, Indexer, IndexMode, InscriptionIndexer, LockIndexer, MapIndexer, OneSatIndexer, OneSatWebSPV, OrdLockIndexer, OriginIndexer, SigmaIndexer } from 'spv-store';
+import { Bsv20Indexer, Bsv21Indexer, CosignIndexer, FundIndexer, Indexer, IndexMode, InscriptionIndexer, LockIndexer, MapIndexer, OneSatIndexer, OneSatWebSPV, OrdLockIndexer, OriginIndexer, SigmaIndexer } from 'spv-store';
 
 const spvProviders: Map<string, OneSatWebSPV> = new Map();
 
@@ -10,12 +10,14 @@ export async function createSPV(address: string): Promise<void> {
             new LockIndexer(owners, 'mainnet'),
             new Bsv21Indexer(owners, IndexMode.Trust),
             new Bsv20Indexer(owners, IndexMode.Trust, 'mainnet'),
+            new CosignIndexer(owners, 'mainnet'),
             new OneSatIndexer(owners, 'mainnet'),
             new OrdLockIndexer(owners, 'mainnet'),
             new InscriptionIndexer(owners, 'mainnet'),
             new MapIndexer(owners, 'mainnet'),
             new SigmaIndexer(owners, 'mainnet'),
             new OriginIndexer(owners, 'mainnet'),
+            new LockIndexer(owners, 'mainnet'),
         ];
 
         spvProviders.set(address, await OneSatWebSPV.init(`${address}-bitvault`, indexers, owners));
